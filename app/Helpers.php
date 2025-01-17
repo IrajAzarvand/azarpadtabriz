4098567509bdfbacbf14dacf7adbebcc936a2b82
Iraj <iraj.azarvand@gmail.com>
2025-01-17 19:43:14 +0330

    add locale middleware, add helper file, update composer

---
+++ app/Helpers.php
@@ -0,0 +1,34 @@
+<?php
+
+function SiteLang()
+{
+    return [
+        'FA' => [
+            'name' => 'فارسی',
+            'rtl' => 1,
+        ],
+        'EN' => [
+            'name' => 'انگلیسی',
+            'rtl' => 0,
+        ],
+        'RU' => [
+            'name' => 'روسی',
+            'rtl' => 0,
+        ],
+        'AR' => [
+            'name' => 'عربی',
+            'rtl' => 1,
+        ],
+    ];
+}
+
+
+/**
+ * set default locale to first item in locale file in config. => fa
+ *
+ * @return string
+ */
+function DefaultLocale()
+{
+    return "FA";
+}
