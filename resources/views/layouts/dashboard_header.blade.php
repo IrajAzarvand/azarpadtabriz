428250ec9f87b8c853e8cb597c1992db3eda9be0
Iraj Azarvand <iraj.azarvand@gmail.com>
2025-01-27 09:30:18 +0330

    fix showing right css & js in other pages of dashboard

--- resources/views/layouts/dashboard_header.blade.php
+++ resources/views/layouts/dashboard_header.blade.php
@@ -7,15 +7,15 @@
 
 
     <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{asset('plugins/font-awesome/css/font-awesome.min.css')}}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
     <!-- bootstrap rtl -->
-    <link rel="stylesheet" href="dist/css/bootstrap-rtl.min.css">
+    <link rel="stylesheet" href="{{asset('dist/css/bootstrap-rtl.min.css')}}">
     <!-- template rtl version -->
-    <link rel="stylesheet" href="dist/css/custom-style.css">
+    <link rel="stylesheet" href="{{asset('dist/css/custom-style.css')}}">
 
 </head>
