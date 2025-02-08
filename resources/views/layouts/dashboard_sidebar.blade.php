341bb100b3b6d469fdec2c9069aa76bba5c593ec
Iraj Azarvand <iraj.azarvand@gmail.com>
2025-02-08 16:04:10 +0330

    show logged in user name in dashboard

--- resources/views/layouts/dashboard_sidebar.blade.php
+++ resources/views/layouts/dashboard_sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="" class="brand-link">
-        <img src="{{ asset('storage/dashboard_images/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+        <img src="{{ asset('storage/dashboard_images/admin_img.png') }}" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">پنل مدیریت</span>
     </a>
@@ -13,11 +13,10 @@
             <!-- Sidebar user panel (optional) -->
             <div class="pb-3 mt-3 mb-3 user-panel d-flex">
                 <div class="image">
-                    <img src="{{ asset('storage/dashboard_images/admin_img.jpg') }}" class="img-circle elevation-2"
-                        alt="User Image">
+
                 </div>
                 <div class="info">
-                    <a href="#" class="d-block">مدیر سایت</a>
+                    <a href="#" class="d-block">{{ Auth::user()->name }}</a>
                 </div>
             </div>
 
