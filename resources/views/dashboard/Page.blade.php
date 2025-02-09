dc98c50b0cde04ec08cfcc534318fd501ace8dad
Iraj Azarvand <iraj.azarvand@gmail.com>
2025-02-09 11:55:52 +0330

    working on dashboard slider section

--- resources/views/dashboard/Page.blade.php
+++ resources/views/dashboard/Page.blade.php
@@ -10,40 +10,41 @@
         <!-- general form elements -->
         <div class="card card-primary">
             <div class="card-header">
-                <h3 class="card-title">مثال ساده</h3>
+                <h3 class="card-title">اسلایدر</h3>
             </div>
+
             <!-- /.card-header -->
             <!-- form start -->
-            <form role="form">
-                <div class="card-body">
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">آدرس ایمیل</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1"
-                            placeholder="ایمیل را وارد کنید">
+                <form role="form">
+                    <div class="card-body">
+                        <table id="simple_paginate" class="table table-bordered table-striped">
+                            <thead>
+                            <tr>
+                                <th>موتور رندر</th>
+                                <th>مرورگر</th>
+                                <th>سیستم عامل</th>
+                                <th>ورژن</th>
+                                <th>امتیاز</th>
+                                <th>2امتیاز</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>Trident</td>
+                                <td>Internet
+                                    Explorer 4.0
+                                </td>
+                                <td>Win 95+</td>
+                                <td> 4</td>
+                                <td>X</td>
+                                <td>X</td>
+                            </tr>
+
+                            </tbody>
+
+                        </table>
                     </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1"
-                            placeholder="پسورد را وارد کنید">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputFile">ارسال فایل</label>
-                        <div class="input-group">
-                            <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="exampleInputFile">
-                                <label class="custom-file-label" for="exampleInputFile">انتخاب فایل</label>
-                            </div>
-                            <div class="input-group-append">
-                                <span class="input-group-text" id="">Upload</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">مرا بخاطر بسپار</label>
-                    </div>
-                </div>
-                <!-- /.card-body -->
+                    <!-- /.card-body -->
 
                 <div class="card-footer">
                     <button type="submit" class="btn btn-primary">ارسال</button>
--- resources/views/layouts/dashboard_scripts.blade.php
+++ resources/views/layouts/dashboard_scripts.blade.php
@@ -5,3 +5,34 @@
 <script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- AdminLTE App -->
 <script src="{{asset('dist/js/adminlte.js')}}"></script>
+<!-- DataTables -->
+<script src="{{asset('plugins/datatables/jquery.dataTables.js')}}"></script>
+<script src="{{asset('plugins/datatables/dataTables.bootstrap4.js')}}"></script>
+
+<script>
+    $(function () {
+        $("#simple_paginate").DataTable({
+            "language": {
+                "paginate": {
+                    "next": "بعدی",
+                    "previous" : "قبلی"
+                }
+            },
+            "info" : false,
+        });
+    // $('#simple_paginate').DataTable({
+    //     "language": {
+    //         "paginate": {
+    //             "next": "بعدی",
+    //             "previous" : "قبلی"
+    //         }
+    //     },
+    //     "info" : false,
+    //     "paging": true,
+    //     "lengthChange": false,
+    //     "searching": false,
+    //     "ordering": true,
+    //     "autoWidth": false
+    // });
+    });
+</script>
