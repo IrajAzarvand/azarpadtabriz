43c391cead07dde15db59ca7335bc8de78c35809
Iraj <iraj.azarvand@gmail.com>
2025-02-10 10:43:02 +0330

    add slider mrs

--- resources/views/dashboard/Page.blade.php
+++ resources/views/dashboard/Page.blade.php
@@ -10,7 +10,7 @@
         <!-- general form elements -->
         <div class="card card-primary">
             <div class="card-header">
-                <h3 class="card-title">اسلایدر</h3>
+                <h3 class="card-title">اسلایدر (اندازه بهینه ۱۹۵۰*۱۳۰۰ حجم کمتر از ۱۰۰ کیلوبایت)</h3>
             </div>
 
             <!-- /.card-header -->
