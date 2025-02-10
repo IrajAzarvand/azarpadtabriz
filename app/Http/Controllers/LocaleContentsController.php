1500a1be8313943dc22776ed2735c8bfc1cd9457
Iraj <iraj.azarvand@gmail.com>
2025-02-10 11:00:46 +0330

    add locale contents mrs

---
+++ app/Http/Controllers/LocaleContentsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LocaleContents;
+use Illuminate\Http\Request;
+
+class LocaleContentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LocaleContents $localeContents)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LocaleContents $localeContents)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, LocaleContents $localeContents)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LocaleContents $localeContents)
+    {
+        //
+    }
+}
