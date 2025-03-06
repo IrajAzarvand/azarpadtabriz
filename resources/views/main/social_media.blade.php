d1b1108e77e4e3f0b5fa12a22f79e961e667ac1f
Iraj Azarvand <iraj.azarvand@gmail.com>
2025-03-06 10:37:03 +0330

    social media done

--- resources/views/main/social_media.blade.php
+++ resources/views/main/social_media.blade.php
@@ -3,56 +3,38 @@
     <div class="pt-80 pb-80 position-relative z-index-2 bg__style text-center lazyload"
         data-bg="img/demo_freelance_portfolio/1920x390_1.jpg" data-brk-library="component__social_block">
         <span class="brk-bg-gradient-40deg-80 brk-abs-overlay"></span>
-        <div class="container all-light">
+        <div class="container" >
             <div class="row mt-30">
-                <div class="col-6 col-md-4 col-lg-2">
+                <div class="col-6 col-md-4 col-lg-3">
                     <div class="social__icon-round">
-                        <a href="#" class="icon-wrap"><i class="brk-icon fab fa-facebook-f"></i></a>
+                        <a href="https://www.facebook.com/share/1VvynLZMjD/" class="icon-wrap"><i class="brk-icon fab fa-facebook-f"></i></a>
                         <h4 class="font__family-montserrat font__weight-light font__size-28 text-white">
                             Facebook</h4>
-                        <p class="font__family-montserrat font__weight-medium font__size-16">Follow Now</p>
                     </div>
                 </div>
-                <div class="col-6 col-md-4 col-lg-2">
+
+                <div class="col-6 col-md-4 col-lg-3">
                     <div class="social__icon-round">
-                        <a href="#" class="icon-wrap"><i class="brk-icon fab fa-twitter"></i></a>
-                        <h4 class="font__family-montserrat font__weight-light font__size-28 text-white">
-                            Twitter</h4>
-                        <p class="font__family-montserrat font__weight-medium font__size-16">Follow Now</p>
-                    </div>
-                </div>
-                <div class="col-6 col-md-4 col-lg-2">
-                    <div class="social__icon-round">
-                        <a href="#" class="icon-wrap"><i class="brk-icon fab fa-instagram"></i></a>
+                        <a href="https://www.instagram.com/azarpadtabriz.co?utm_source=qr&igsh=MXdiOGZha2pvZmRnbQ==" class="icon-wrap"><i class="brk-icon fab fa-instagram"></i></a>
                         <h4 class="font__family-montserrat font__weight-light font__size-28 text-white">
                             Instagram</h4>
-                        <p class="font__family-montserrat font__weight-medium font__size-16">Follow Now</p>
                     </div>
                 </div>
-                <div class="col-6 col-md-4 col-lg-2">
+                <div class="col-6 col-md-4 col-lg-3">
                     <div class="social__icon-round">
-                        <a href="#" class="icon-wrap"><i class="brk-icon fab fa-youtube"></i></a>
+                        <a href="https://wa.me/qr/3TUC7RCY4IZMA1" class="icon-wrap"><i class="brk-icon fab fa-whatsapp"></i></a>
                         <h4 class="font__family-montserrat font__weight-light font__size-28 text-white">
-                            YouTube</h4>
-                        <p class="font__family-montserrat font__weight-medium font__size-16">Follow Now</p>
+                            WhatsApp</h4>
                     </div>
                 </div>
-                <div class="col-6 col-md-4 col-lg-2">
+                <div class="col-6 col-md-4 col-lg-3">
                     <div class="social__icon-round">
-                        <a href="#" class="icon-wrap"><i class="brk-icon fab fa-google"></i></a>
+                        <a href="https://t.me/azarpadtabriz" class="icon-wrap"><i class="brk-icon fab fa-telegram"></i></a>
                         <h4 class="font__family-montserrat font__weight-light font__size-28 text-white">
-                            Google +</h4>
-                        <p class="font__family-montserrat font__weight-medium font__size-16">Follow Now</p>
+                            Telegram</h4>
                     </div>
                 </div>
-                <div class="col-6 col-md-4 col-lg-2">
-                    <div class="social__icon-round">
-                        <a href="#" class="icon-wrap"><i class="brk-icon fab fa-vimeo-v"></i></a>
-                        <h4 class="font__family-montserrat font__weight-light font__size-28 text-white">
-                            Vimeo</h4>
-                        <p class="font__family-montserrat font__weight-medium font__size-16">Follow Now</p>
-                    </div>
-                </div>
+
             </div>
         </div>
     </div>
