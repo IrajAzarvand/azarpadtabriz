72100623d093c7172180ca16a830123bbd27e11a
Iraj Azarvand <iraj.azarvand@gmail.com>
2025-05-08 15:19:48 +0330

    create blog comments mrs

--- database/migrations/2025_05_08_114140_create_blogs_table.php
+++ database/migrations/2025_05_08_114140_create_blogs_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('blog_comments', function (Blueprint $table) {
+        Schema::create('blogs', function (Blueprint $table) {
             $table->id();
+            $table->string('image')->nullable();
+            $table->string('tags')->nullable();
+            $table->string('comments')->nullable();
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('blog_comments');
+        Schema::dropIfExists('blogs');
     }
 };
